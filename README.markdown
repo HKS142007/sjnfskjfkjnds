d776d02d8e01b84bfc9b4ccd3223013b543afbf9
HKS142007 <74846505+HKS142007@users.noreply.github.com>
2021-02-25 16:01:26 +0400

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# macintosh.css
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jsdf/pen/rNOLEqY](https://codepen.io/jsdf/pen/rNOLEqY).
+
+Tutorial: https://fossheim.io/writing/posts/css-macintosh/
---
+++ src/script.js
@@ -0,0 +1,24 @@
+// add a loading progress bar. not required, but good ux
+var loadingStatus = PCEJSUtil.loadingStatus(
+  document.querySelector('.pcejs-loading-status')
+);
+
+window.macplus = PCEJSMacplus({
+  arguments: ['-c', 'pce-config.cfg', '-r'],
+  autoloadFiles: [
+    'https://unpkg.com/pcejs-macplus@0.1.7/macplus-pcex.rom',
+    'https://jamesfriend.com.au/pce-js/codepen/mac-plus.rom',
+    'https://jamesfriend.com.au/pce-js/codepen/hd1.qed',
+    'https://jamesfriend.com.au/pce-js/codepen/pce-config.cfg',
+  ],
+  locateFile(path, scriptDirectory) {
+    return "https://unpkg.com/pcejs-macplus@0.1.7/" + path;
+  },
+  print: console.log.bind(console),
+  printErr: console.warn.bind(console),
+  canvas: document.querySelector('.pcejs-canvas'),
+  monitorRunDependencies: function(remainingDependencies) {
+    loadingStatus.update(remainingDependencies);
+  },
+});
+
